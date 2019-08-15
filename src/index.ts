c07c386702759860fb84328d44aea250b7437a0f
ninox92 <thomasreynders@hotmail.nl>
2019-08-15 14:58:19 +0200

    fix(StateMachine): Add types to statemachine

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,132 @@
-export * from './statemachine';
+'use strict'
+
+interface IStateNode {
+  name: string
+}
+
+type StateAction = Function | StateTransition
+
+interface IStateMachineDescription {
+  [key: string]: Object | string | StateAction
+}
+
+class StateTransition {
+  public stateName: string
+  public operation: Function
+
+  constructor(stateName: string, fn: Function) {
+    this.stateName = stateName
+    this.operation = fn
+  }
+
+  public transition(scope: StateMachine) {
+    scope.setState(this.stateName)
+  }
+}
+
+class StateTrigger implements IStateNode {
+  public name: string
+  private _action: StateAction
+
+  constructor(name: string, action: StateAction) {
+    this.name = name
+    this._action = action
+  }
+
+  public call(stateMachine: StateMachine, ...args: any[]) {
+    let returnValue: any
+    args.unshift({ scope: stateMachine })
+    if (this._action instanceof StateTransition) {
+      returnValue = this._action.operation.apply(this, args)
+      this._action.transition(stateMachine)
+    } else {
+      returnValue = this._action.apply(this, args)
+    }
+
+    return returnValue
+  }
+}
+
+class State implements IStateNode {
+  public name: string
+  public triggers: StateTrigger[] = []
+
+  constructor(name: string, triggersDescription: object) {
+    this.name = name
+    const triggers = Object.entries(triggersDescription)
+    for (const [triggerName, triggerAction] of triggers) {
+      this.triggers.push(new StateTrigger(triggerName, triggerAction))
+    }
+  }
+
+  public getEventNames(): string[] {
+    return this.triggers.map((event: StateTrigger) => event.name)
+  }
+}
+
+class StateMachine {
+  static STATES: string = 'states'
+  static STARTING_STATE: string = 'starting-state'
+
+  public previousStateName: string | undefined
+  public stateName: string | undefined
+  private states: Map<string, State>
+
+  [key: string]: any
+
+  constructor(description: IStateMachineDescription) {
+    const startingStateName: string = description[StateMachine.STARTING_STATE] as string
+    const RESERVED: string[] = [StateMachine.STARTING_STATE, StateMachine.STATES]
+    const propertiesAndMethods = Object.keys(description).filter(
+      property => !RESERVED.includes(property)
+    )
+    for (const property of propertiesAndMethods) {
+      this[property] = description[property] as Object
+    }
+
+    this.states = new Map<string, State>()
+    const _states = Object.entries(description[StateMachine.STATES])
+    for (const [stateName, triggers] of _states) {
+      this.states.set(stateName, new State(stateName, triggers as Object))
+    }
+
+    this.setState(startingStateName)
+  }
+
+  public getState(): State | undefined {
+    return this.stateName === undefined ? undefined : this.states!.get(this.stateName)
+  }
+
+  public getEventNames(): string[] {
+    const state = this.getState()
+    return state === undefined ? [] : state!.getEventNames()
+  }
+
+  public getAllStatesNames(): string[] {
+    return [...this.states.keys()]
+  }
+
+  public setState(stateName: string): void {
+    const currentState = this.getState()
+    if (currentState) {
+      for (const { name } of currentState.triggers) {
+        if (typeof this.__proto__[name] === 'function') {
+          delete this.__proto__[name]
+        }
+      }
+    }
+
+    // Set the new state
+    this.previousStateName = this.stateName
+    this.stateName = stateName
+
+    const state = this.getState()!
+    for (const event of state.triggers) {
+      this.__proto__[event.name] = function(...args: any[]) {
+        event.call(this, ...args)
+      }
+    }
+  }
+}
+
+export { StateMachine, StateTransition, IStateMachineDescription }
--- test/statemachine.test.ts
+++ test/statemachine.test.ts
@@ -1,43 +1,95 @@
-declare let it, expect;
+declare let describe: jest.Describe
+declare let it: jest.It
+declare let expect: jest.Expect
 
-import {StateMachine, StateTransition} from '../src/statemachine';
+import { StateMachine, StateTransition, IStateMachineDescription } from '../src'
 
-const description = {
-    balance: 0,
-    [StateMachine.STARTING_STATE]: 'open',
-    [StateMachine.STATES]: {
-      'open': {
-        'deposit' (amount: number) {
-          this.balance = this.balance + amount; 
-          console.log(`Add balance ${amount}`, this);
-        },
-        'withdraw' (amount: number) { this.balance = this.balance - amount; },
-        'placeHold': new StateTransition('held', () : any => undefined),
-        'close': new StateTransition('closed', function () {
-          if (this.balance > 0) {
-            // ...transfer balance to suspension account
-          }
-        })
+interface BankStateMachineDescription extends IStateMachineDescription {
+  balance: number
+}
+
+const description: BankStateMachineDescription = {
+  balance: 0,
+  [StateMachine.STARTING_STATE]: 'open',
+  [StateMachine.STATES]: {
+    open: {
+      deposit({ scope }: { scope: BankStateMachineDescription }, amount: number) {
+        scope.balance += amount
       },
-      'held': {
-        'removeHold': new StateTransition('open', () : any => undefined),
-        'deposit' (amount: number) { this.balance = this.balance + amount; },
-        'close': new StateTransition('closed', function () {
-          if (this.balance > 0) {
-            // ...transfer balance to suspension account
-          }
-        })
+      withdraw({ scope }: { scope: BankStateMachineDescription }, amount: number) {
+        scope.balance -= amount
       },
-      'closed': {
-        'reopen': new StateTransition('open', function () {
-          // ...restore balance if applicable
-        })
-      }
+      placeHold: new StateTransition('held', (): any => undefined),
+      close: new StateTransition('closed', function({
+        scope
+      }: {
+        scope: BankStateMachineDescription
+      }) {
+        if (scope.balance > 0) {
+          // ...transfer balance to suspension account
+        }
+      })
+    },
+    held: {
+      removeHold: new StateTransition('open', (): any => undefined),
+      deposit({ scope }: { scope: BankStateMachineDescription }, amount: number) {
+        scope.balance += amount
+      },
+      close: new StateTransition('closed', function({
+        scope
+      }: {
+        scope: BankStateMachineDescription
+      }) {
+        if (scope.balance > 0) {
+          // ...transfer balance to suspension account
+        }
+      })
+    },
+    closed: {
+      reopen: new StateTransition('open', function() {
+        // ...restore balance if applicable
+      })
     }
-};
+  }
+}
 
-it('should create a new StateMachine', () => {
+describe('Testing StateMachine', () => {
+  it('should be able to create a new instance of StateMachine successfully', () => {
+    const statemachine = new StateMachine(description)
+    expect(statemachine).toBeInstanceOf(StateMachine)
+  })
 
-    
+  it('should be able to deposit an amount succesfully', () => {
+    const statemachine = new StateMachine(description)
+    statemachine.deposit(5)
 
-});
+    const scope = (statemachine as unknown) as BankStateMachineDescription
+    expect(scope.balance).toBe(5)
+  })
+
+  it('should be able to withdraw an amount succesfully', () => {
+    const statemachine = new StateMachine(description)
+    statemachine.withdraw(5)
+
+    const scope = (statemachine as unknown) as BankStateMachineDescription
+    expect(scope.balance).toBe(-5)
+  })
+
+  it('should be able to close an account succesfully', () => {
+    const statemachine = new StateMachine(description)
+    statemachine.close()
+
+    expect(statemachine.previousStateName).toBe('open')
+    expect(statemachine.stateName).toBe('closed')
+  })
+
+  it('should be able to reopen an account succesfully', () => {
+    const statemachine = new StateMachine(description)
+
+    statemachine.close()
+    statemachine.reopen()
+
+    expect(statemachine.previousStateName).toBe('closed')
+    expect(statemachine.stateName).toBe('open')
+  })
+})
